eb2bfb285fe0eceb1c3bdfc9a10cdbbf0a3f2c06
secret42 <secret42@qq.com>
2021-01-17 21:24:00 +0800

    定义初始变量

---
+++ pso.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 0x3f3f3f3f;
+const double pi = acos(-1.0);
+struct PSO{
+    //dim:维数,p_num:粒子数量,iters:迭代次数
+    int dim,p_num,iters;
+    double v_max,v_min,pos_max,pos_min;
+    vector<double> pos,spd,p_best;
+    double g_best;
+
+};
+int main(){
+    
+    return 0;
+}
