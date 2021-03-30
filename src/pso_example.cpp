7061d12482d6b1eeaab678dae2e3a2f79837a9bd
secret42 <secret42@qq.com>
2021-03-30 14:40:59 +0800

    feature:注释不需要的部分

--- src/pso_example.cpp
+++ src/pso_example.cpp
@@ -1,75 +1,75 @@
-#include "pso.h"
+// #include "pso.h"
 
 
-struct PSO{
-    //dim:维数,pum:粒子数量,iters:迭代次数,
-    int dim,pNum,iters;
-    double v_max,v_min,pos_max,pos_min;
-    vector<double> pos,spd,pBest;
-    double gBest;
-    Matrix<double, Dynamic, Dynamic> fTest;
-    Matrix<double, Dynamic, Dynamic> posMat;   
+// struct PSO{
+//     //dim:维数,pum:粒子数量,iters:迭代次数,
+//     int dim,pNum,iters;
+//     double v_max,v_min,pos_max,pos_min;
+//     vector<double> pos,spd,pBest;
+//     double gBest;
+//     Matrix<double, Dynamic, Dynamic> fTest;
+//     Matrix<double, Dynamic, Dynamic> posMat;   
 
-    double calCost(double x){
-        double res = x * x + 1;
-        return res;
-    }
+//     double calCost(double x){
+//         double res = x * x + 1;
+//         return res;
+//     }
 
-    void init(int _dim,int _pNum,int _iters = 100000){
-        posMat.resize(_iters,pNum); posMat.fill(INF);
-        fTest.resize(_iters,pNum);  fTest.fill(INF);            
-        static mt19937 rng;
-        uniform_real_distribution<double> rand1(-1,2);
-        uniform_real_distribution<double> rand2(-2,4);
-        for(int i = 1; i < pNum; ++i){
-            pos.push_back(rand1(rng));
-            spd.push_back(rand2(rng));
-        }
-        vector<double> vec;
-        for(int i = 0; i < pNum; ++i){  //初始化各个粒子的历史最优位置
-            double tmp = calCost(pos[i]);
-            fTest(0,i) = tmp;
-            posMat(0,i) = pos[i];
-            pBest.push_back(pos[i]);
-        }
-        Index  minRow,minCol;
-        fTest.row(0).minCoeff(&minRow,&minCol);
-        gBest = posMat(minRow,minCol);// 初始话全局最优解
-    }
+//     void init(int _dim,int _pNum,int _iters = 100000){
+//         posMat.resize(_iters,pNum); posMat.fill(INF);
+//         fTest.resize(_iters,pNum);  fTest.fill(INF);            
+//         static mt19937 rng;
+//         uniform_real_distribution<double> rand1(-1,2);
+//         uniform_real_distribution<double> rand2(-2,4);
+//         for(int i = 1; i < pNum; ++i){
+//             pos.push_back(rand1(rng));
+//             spd.push_back(rand2(rng));
+//         }
+//         vector<double> vec;
+//         for(int i = 0; i < pNum; ++i){  //初始化各个粒子的历史最优位置
+//             double tmp = calCost(pos[i]);
+//             fTest(0,i) = tmp;
+//             posMat(0,i) = pos[i];
+//             pBest.push_back(pos[i]);
+//         }
+//         Index  minRow,minCol;
+//         fTest.row(0).minCoeff(&minRow,&minCol);
+//         gBest = posMat(minRow,minCol);// 初始话全局最优解
+//     }
 
-    void psoAlgorithm(){
-        static mt19937 rng;
-        uniform_real_distribution<double> randNum(0,1);
-        for(int step = 1; step <= iters; ++step){
-            for(int i = 0; i < pNum; ++i){
-                spd[i] = 0.5 * spd[i]   + 2 * randNum(rng) * (pBest[i] - pos[i])
-                                        + 2 * randNum(rng) * (gBest - pos[i]);
-                pos[i] = pos[i] + spd[i];
-                //越界处理
-                if (spd[i] < -2 || spd[i] > 4)
-                    spd[i] = 4;
-                if (pos[i] < -1 || pos[i] > 2)
-                    pos[i] = -1;
-                posMat(step,i) = pos[i];
-            }
-            //更新函数值矩阵
-            for (int i = 0; i < pNum; ++i){
-                auto tmp = calCost(pos[i]);
-                fTest(step, i) = tmp;
-            } 
-            for(int i = 0; i < pNum; ++i){
-                MatrixXd tmpBest;
-                tmpBest = fTest.col(i);
-                Index minRow,minCol;
-                tmpBest.minCoeff(&minRow,&minCol);
-                pBest[i] = posMat(minRow,i);
-            }
-            gBest = *min_element(pBest.begin(),pBest.end());
-        }
-    }
+//     void psoAlgorithm(){
+//         static mt19937 rng;
+//         uniform_real_distribution<double> randNum(0,1);
+//         for(int step = 1; step <= iters; ++step){
+//             for(int i = 0; i < pNum; ++i){
+//                 spd[i] = 0.5 * spd[i]   + 2 * randNum(rng) * (pBest[i] - pos[i])
+//                                         + 2 * randNum(rng) * (gBest - pos[i]);
+//                 pos[i] = pos[i] + spd[i];
+//                 //越界处理
+//                 if (spd[i] < -2 || spd[i] > 4)
+//                     spd[i] = 4;
+//                 if (pos[i] < -1 || pos[i] > 2)
+//                     pos[i] = -1;
+//                 posMat(step,i) = pos[i];
+//             }
+//             //更新函数值矩阵
+//             for (int i = 0; i < pNum; ++i){
+//                 auto tmp = calCost(pos[i]);
+//                 fTest(step, i) = tmp;
+//             } 
+//             for(int i = 0; i < pNum; ++i){
+//                 MatrixXd tmpBest;
+//                 tmpBest = fTest.col(i);
+//                 Index minRow,minCol;
+//                 tmpBest.minCoeff(&minRow,&minCol);
+//                 pBest[i] = posMat(minRow,i);
+//             }
+//             gBest = *min_element(pBest.begin(),pBest.end());
+//         }
+//     }
 
 
-};
+// };
 
 
 
