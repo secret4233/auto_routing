12c592a797d6073062bfcc4239e92a7a534d8ccb
secret42 <secret42@qq.com>
2021-03-30 14:45:44 +0800

    debug:准备进行调试

--- main.cpp
+++ main.cpp
@@ -12,12 +12,18 @@ using namespace std;
 int main(){
     LogUtils logger(ALL_LOG_LEVEL);
     
-    int lineNum = 50; 
-    AStar method1(lineNum * lineNum,1);
-    int start = method1.GetGraphVertexNum(30,0);
-    int end = method1.GetGraphVertexNum(47,lineNum-1);
-    double ans = method1.CoreAlgorithm(start,end);
+    // int lineNum = 50; 
+    // AStar method1(lineNum * lineNum,1);
+    // int start = method1.GetGraphVertexNum(30,0);
+    // int end = method1.GetGraphVertexNum(47,lineNum-1);
+    // double ans = method1.CoreAlgorithm(start,end);
 
-    printf("%d -> %d %lf\n",start,end,ans);
+    // printf("%d -> %d %lf\n",start,end,ans);
+
+    PSOAlgorithm method2(10,1000);
+
+    method2.CoreAlgorithm();
+    method2.PrintAlgorithmAns();
+
     return 0;
 }
